9bbb0245a9c3c79acb2deade58756522507bc550
Thiago Alves <thiago.ndev@gmail.com>
2023-11-11 04:00:46 -0300

    fix-adicionado-carrossel

---
+++ aula.py
@@ -0,0 +1,79 @@
+import site
+import time
+
+
+def converter_moeda(qtd: float, cot: float):
+    valor = qtd * cot
+    return valor
+
+
+E = float(input("Digite a quantia em Euros: "))
+C = float(input("Digite a Cotação: "))
+
+print("O valor em Reais é: ", converter_moeda(E, C))
+
+
+def fib(n: int):
+    if n < 2:
+        return n
+    else:
+        chamada = 0
+        chamada += 1
+        print(f'chamada: {chamada}')
+        return fib(n - 1) + fib(n - 2)
+
+
+X = fib(4)
+
+print(X)
+
+
+def adicao(a, b):
+    return a + b
+
+
+def media(a, b):
+    return adicao(a, b) / 2
+
+
+nota = media(10, 20)
+print(nota)
+#
+#
+inicio = time.time()
+
+
+def fat(n):
+    lista = []
+    if n < 2:
+        return n
+    else:
+        for numer in range(n - 1, 0, -1):
+            lista.append(numer)
+        v = 0
+        t = len(lista)
+        while t != 1:
+            if v == 0:
+                x = n * t
+                v = x
+                t -= 1
+                if n == 3:
+                    return v
+            if v > 0:
+                result = v * t
+                v = result
+                t -= 1
+        return v
+
+
+# def fat(n):
+#     if n == 0 or n ==1:
+#         return 1
+#     else:
+#         return n * fat(n-1)
+
+print(fat(900))
+fim = time.time()  # calcula o tempo de execução
+
+tempo = fim - inicio
+print(f"tempo de execução: {tempo:.10f} segundos")
--- templates/static/asset/js/menu.js
+++ templates/static/asset/js/menu.js
@@ -3,7 +3,13 @@ console.log(1)
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu')
-
+let btnEnviar = document.querySelector(".btn-enviar")
+let inputNome = document.getElementById('1')
+let inputEmail = document.getElementById('2')
+let inputNumero = document.getElementById('3')
+let inputServico = document.getElementById('4')
+let portifolio = document.querySelector(".portifolio")
+const img = document.querySelector(".img-port")
 
 btnMenu.addEventListener('click', ()=>{
     menu.classList.add('abrir-menu')
@@ -17,4 +23,51 @@ overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
 
+btnEnviar.addEventListener('click', function(e){
+    e.preventDefault();
+    const nome = inputNome.value
+    const email = inputEmail.value
+    const numero = inputNumero.value
+    const servico = inputServico.value
 
+    window.alert(`Obrigado ${nome} sua solicitação foi recebida.`)
+
+})
+
+
+
+const controls = document.querySelectorAll('.control');
+let currentItem = 0
+const items = document.querySelectorAll('.img-port');
+const maxItems = items.length;
+
+
+controls.forEach(control => {
+    control.addEventListener('click', (e) =>{
+        const  isLeft = e.target.classList.contains("arrow-left");
+
+        if (isLeft) {
+            currentItem -= 1;
+        } else {
+            currentItem += 1;
+        }
+
+        if (currentItem >= maxItems) {
+            currentItem = 0;
+        }
+        if (currentItem < 0) {
+            currentItem = maxItems - 1;
+        }
+
+        console.log('control', isLeft, currentItem, maxItems)
+
+        items.forEach((item) => item.classList.remove("current-item"));
+
+    items[currentItem].scrollIntoView({
+      behavior: "smooth",
+      inline: "center"
+    });
+
+    items[currentItem].classList.add("current-item");
+  });
+});
